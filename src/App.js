7ddd825edb72753e8397b0d53825f5aa89bdea5d
Caio Cesar Dantas da Silva <caio.cdantas@NTLD277662.prevnet>
2018-09-14 15:58:05 -0300

    priemiro commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from './componentes/Header';
+import Timeline from './componentes/Timeline';
+import './css/reset.css'
+import './css/timeline.css'
+
+class App extends Component {
+  render() {
+    return (
+    <div id="root">
+      <div className="main">
+        <Header/>
+        <Timeline/>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
